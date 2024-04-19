471243ae4c94723a71b3411141688eca5cdefaf3
avinashgottam <gottamavinash@gmail.com>
2024-04-20 02:35:37 +0530

    3rd change

--- 3rd script.sh
+++ 3rd script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 x1=$1
 y2=$2
- echo "x1=hii ra vamshi"
- echo "y2=hii ra y2 ela unnav"
- echo "x1=super ra y2"
- 
+ echo "$x1=hii ra vamshi"
+ echo "$y2=hii ra $y2 ela unnav"
+ echo "$x1=super ra $y2"
