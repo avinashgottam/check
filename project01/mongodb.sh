1edffa2921f19cbfc80546b8144a8a6f9cc9d4d9
avinashgottam <gottamavinash@gmail.com>
2024-04-21 03:49:03 +0530

    mongodb

--- project01/mongodb.sh
+++ project01/mongodb.sh
@@ -17,7 +17,7 @@ VALIDATE(){
         echo -e "Error:$2 ..$R Failed $N"
         exit 1
     else
-        echo -e "$2 ... $G Success $N"
+        echo -e "$2... $G Success $N"
     fi      
 }
 if [ $ID -ne 0 ]
@@ -27,6 +27,23 @@ then
 else
     echo -e "root user"
 fi
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copied mongorepo"
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "installing moongodb"
+systemctl enable mongod
+VALIDATE $? "enable moongodb"
+systemctl start mongod
+VALIDATE $? "starting moongodb"
+sed -e 's/127.0.0.0/0.0.0.' /etc/mongod.conf &>> $LOGFILE
+sytemctl restart mongod
+VALIDATE $? "restarting mongodb"
+
+
+
+
 
 
 
