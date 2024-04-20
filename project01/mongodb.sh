17d85879ebe13e4cb1979f400b5bb0b85a513f58
avinashgottam <gottamavinash@gmail.com>
2024-04-21 03:20:53 +0530

    learining conditions

--- project01/mongodb.sh
+++ project01/mongodb.sh
@@ -6,14 +6,27 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGFILE=
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if ($1 ne 0)
     then
-        echo -e "Error:$2 .. Failed"
+        echo -e "Error:$2 ..$R Failed $N"
         exit 1
     else
-        echo -e "$2 ... Success
+        echo -e "$2 ... $G Success $N"
     fi      
 }
+if [ $ID -ne 0 ]
+then
+    echo -e "Your are not root user"
+    exit 1
+else
+    echo -e "root user"
+fi
+
+
+
