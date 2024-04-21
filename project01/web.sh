f4624b430bd10fe517cd7d3adbc76efc90a8cf91
avinashgottam <164002012+avinashgottam@users.noreply.github.com>
2024-04-22 01:11:50 +0530

    Update web.sh

--- project01/web.sh
+++ project01/web.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo -e " Error $2 :: FAILURE"
     else
